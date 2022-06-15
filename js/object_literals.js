384f3df7f5548d99be1f857c7f92ab30f69c6149
srikanthpragada <srikanthpragada@yahoo.com>
2022-06-15 10:28:29 +0530

    15-JUN

--- js/object_literals.js
+++ js/object_literals.js
@@ -15,10 +15,10 @@ print_person(player)
 let p2 = { ...person, mobile: '484883343', email  : 'alex@yahoo.com' };
 console.log(p2)
 
-function display(name, email) {
+function display({name, email}) {
     console.log(name)
     console.log(email)
 }
 
-display()   // To be done 
+display(person)   // To be done 
 
